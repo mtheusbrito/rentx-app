eaec55717f36cffffeb364bcd95f14cd557f5e0b
mtheusbrito <matheusbritodasilva@gmail.com>
2022-01-21 01:39:22 -0300

    Tema Global da Aplicação

--- App.tsx
+++ App.tsx
@@ -1,9 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_500Medium,
+} from '@expo-google-fonts/inter';
+import {
+  Archivo_400Regular,
+  Archivo_500Medium,
+  Archivo_600SemiBold,
+} from '@expo-google-fonts/archivo';
+
+
+
+import React, { useState } from 'react';
 
 import { Home } from './src/screens/Home';
+import AppLoading from 'expo-app-loading';
+import { ThemeProvider } from 'styled-components'
+import theme from './src/styles/theme';
 
 export default function App() {
-  return <Home/>
-}
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Archivo_400Regular,
+    Archivo_500Medium,
+    Archivo_600SemiBold,
+  });
 
+  if (!fontsLoaded) {
+    return <AppLoading/>
+  }
+  return (
+    <ThemeProvider theme={theme}>
+      <Home />
+    </ThemeProvider>
+  );
+}
