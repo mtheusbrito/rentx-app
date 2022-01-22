a42d7ecf96b09f5504341e71d3ad26400aa23da2
mtheusbrito <matheusbritodasilva@gmail.com>
2022-01-21 23:24:12 -0300

    Criação do Header da Home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
-
-import { Container, Title } from './styles';
+import { StatusBar } from 'react-native'
+import { Container, Header, TotalCars, HeaderContent } from './styles';
+import Logo from './../../assets/logo.svg';
+import { RFValue } from 'react-native-responsive-fontsize';
 
 export function Home(){
 return (
- <Container>
-   <Title>Home</Title>
-</Container>
- );
+  <Container>
+    <StatusBar
+      barStyle="light-content"
+      backgroundColor="transparent"
+      translucent
+    />
+   
+      <Header>
+         <HeaderContent>
+        <Logo width={RFValue(108)} height={RFValue(12)} />
+        <TotalCars>Total de 12 carros</TotalCars>
+        </HeaderContent>
+      </Header>
+    
+  </Container>
+);
 }
 
