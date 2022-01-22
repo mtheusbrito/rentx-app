99290aece0629ed142a46795b77fd140d141a314
mtheusbrito <matheusbritodasilva@gmail.com>
2022-01-22 00:49:54 -0300

    Criação do Card Car

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -3,8 +3,18 @@ import { StatusBar } from 'react-native'
 import { Container, Header, TotalCars, HeaderContent } from './styles';
 import Logo from './../../assets/logo.svg';
 import { RFValue } from 'react-native-responsive-fontsize';
+import { Car } from '../../components/Car';
 
 export function Home(){
+  const carData = {
+    brand: 'audi',
+    name: 'RS 5 Coupé',
+    rent: {
+      period: 'Ao dia',
+      price: 120,
+    },
+    thumbnail: 'https://freepngimg.com/thumb/audi/35227-5-audi-rs5-red.png',
+  };
 return (
   <Container>
     <StatusBar
@@ -19,6 +29,8 @@ return (
         <TotalCars>Total de 12 carros</TotalCars>
         </HeaderContent>
       </Header>
+
+      <Car data={carData}/>
     
   </Container>
 );
