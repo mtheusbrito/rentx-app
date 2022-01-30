24dca897b2275f130cb8eaf11c336d4e4a309c43
mtheusbrito <matheusbritodasilva@gmail.com>
2022-01-30 18:50:01 -0300

    Interface de Agendamento

--- App.tsx
+++ App.tsx
@@ -19,6 +19,7 @@ import AppLoading from 'expo-app-loading';
 import { ThemeProvider } from 'styled-components'
 import theme from './src/styles/theme';
 import { CarDetails } from './src/screens/CarDetails';
+import { Scheduling } from './src/screens/Scheduling';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -34,7 +35,7 @@ export default function App() {
   }
   return (
     <ThemeProvider theme={theme}>
-      <CarDetails />
+      <Scheduling />
     </ThemeProvider>
   );
 }
---
+++ src/screens/Scheduling/index.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import { StatusBar } from 'react-native';
+import { useTheme } from 'styled-components';
+import { BackButton } from '../../components/BackButton';
+
+
+import {
+  Container,
+  Header,
+  Title,
+  RentalPeriod,
+  DateInfo,
+  DateTitle,
+  DateValue,
+  DateValueContainer,
+  Content,
+Footer
+} from './styles';
+import ArrowSvg  from '../../assets/arrow.svg'
+import { Button } from '../../components/Button';
+
+
+
+export function Scheduling() {
+  const theme = useTheme();
+  return (
+    <Container>
+      <Header>
+        <StatusBar
+          barStyle="light-content"
+          translucent
+          backgroundColor="transparent"
+        />
+        <BackButton onPress={() => {}} color={theme.colors.shape} />
+        <Title>
+          Escolha uma{`\n`}
+          data de início e{`\n`}
+          fim do aluguel.
+        </Title>
+
+        <RentalPeriod>
+          <DateInfo>
+            <DateTitle>DE</DateTitle>
+            <DateValueContainer selected={false}>
+              <DateValue></DateValue>
+            </DateValueContainer>
+          </DateInfo>
+
+          <ArrowSvg />
+
+          <DateInfo>
+            <DateTitle>ATÉ</DateTitle>
+            <DateValueContainer selected={false}>
+              <DateValue></DateValue>
+            </DateValueContainer>
+          </DateInfo>
+        </RentalPeriod>
+      </Header>
+      <Content></Content>
+      <Footer>
+        <Button title="Confirmar" />
+      </Footer>
+    </Container>
+  );
+}
