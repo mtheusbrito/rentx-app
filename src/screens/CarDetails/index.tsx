a40c29a4bbb7851703b59b31b585a1487be0e88f
mtheusbrito <matheusbritodasilva@gmail.com>
2022-01-30 13:48:28 -0300

    Componente de Acessórios

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
-
+import { Accessory } from '../../components/Accessory';
 import { BackButton } from '../../components/BackButton';
 import { ImageSlider } from '../../components/ImageSlider';
+
+import speedSvg  from '../../assets/speed.svg';
+import accelerationSvg from '../../assets/acceleration.svg';
+
+import forceSvg  from'../../assets/force.svg';
+
+import gasolineSvg  from '../../assets/gasoline.svg';
+
+import exchangeSvg   from'../../assets/exchange.svg';
+
+import  peopleSvg  from '../../assets/people.svg';
+
 
 import {
   Container,
@@ -16,6 +28,7 @@ import {
   Period,
   Price,
   About,
+  Accessories,
 } from './styles';
 
 export function CarDetails() {
@@ -43,13 +56,20 @@ export function CarDetails() {
             <Price>R$ 1000 </Price>
           </Rent>
         </Details>
-          <About>
-            Este é automóvel desportivo. Surgiu do lendário touro de lide indultado na praća Real Maestranza de Sevilla.
-            É um belissimo carro para quem fosta de acelerar.
-            
-            </About>
+        <Accessories>
+          <Accessory name="380km/h" icon={speedSvg}/>
+          <Accessory name='3.2s' icon={accelerationSvg}/>
+          <Accessory name='800 HP' icon={forceSvg}/>
+          <Accessory  name='Gasoline' icon={gasolineSvg}/>
+          <Accessory name='Auto' icon={exchangeSvg}/>
+          <Accessory name='2 peoples' icon={peopleSvg}/>
+        </Accessories>
 
-
+        <About>
+          Este é automóvel desportivo. Surgiu do lendário touro de lide
+          indultado na praća Real Maestranza de Sevilla. É um belissimo carro
+          para quem fosta de acelerar.
+        </About>
       </Content>
     </Container>
   );
