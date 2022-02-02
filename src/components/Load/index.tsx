a8ea7e4869e717a3fd08234095a1267d83f8dbcd
mtheusbrito <matheusbritodasilva@gmail.com>
2022-02-02 09:08:45 -0300

    Listando carros da API

---
+++ src/components/Load/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { ActivityIndicator } from 'react-native';
+import { useTheme } from 'styled-components';
+
+import { Container } from './styles';
+
+export function Load(){
+  const theme = useTheme();
+return (
+  <Container>
+    <ActivityIndicator color={theme.colors.main} size="large" />
+  </Container>
+);
+}
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,36 +1,52 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StatusBar } from 'react-native';
-import { Container, Header, TotalCars, HeaderContent } from './styles';
+import { Container, Header, TotalCars, HeaderContent, CarList } from './styles';
 import Logo from './../../assets/logo.svg';
 import { RFValue } from 'react-native-responsive-fontsize';
 import { Car } from '../../components/Car';
 import { useNavigation } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { RootStackParamList } from '../../@types/navigation';
-
-
+import {api} from '../../service/api';
+import { useTheme } from  'styled-components'
+import { CarDTO } from '../../dtos/CarDTO';
+import { Load } from '../../components/Load';
+ 
 type CarDetailsScreenNavigationProp = NativeStackNavigationProp<
   RootStackParamList,
   'CarDetails'
 >;
 
+
+
 export function Home() {
   const { navigate } = useNavigation<CarDetailsScreenNavigationProp>();
+  const [cars, setCars] = useState<CarDTO[]>([] as CarDTO[]);
+  const [loading, setLoading] = useState<boolean>(true);
+
   
-
-  const carData = {
-    brand: 'audi',
-    name: 'RS 5 Coupé',
-    rent: {
-      period: 'Ao dia',
-      price: 120,
-    },
-    thumbnail: 'https://freepngimg.com/thumb/audi/35227-5-audi-rs5-red.png',
-  };
-
   function handleDetails() {
    navigate('CarDetails', {option: ''});
   }
+
+useEffect(()=>{
+  fetchCars();
+
+
+ async function fetchCars() {
+   try{
+    const { data } = await api.get<CarDTO[]>(`/cars`);
+    setCars(data);
+   }catch(error){
+    console.log(error);
+   }finally{
+     setLoading(false);
+   }
+   
+
+ }
+},[])
+
   return (
     <Container>
       <StatusBar
@@ -42,11 +58,18 @@ export function Home() {
       <Header>
         <HeaderContent>
           <Logo width={RFValue(108)} height={RFValue(12)} />
-          <TotalCars>Total de 12 carros</TotalCars>
+          <TotalCars>{`Total de ${cars.length} ${cars.length === 1 ? `carro` : `carros` }`}</TotalCars>
         </HeaderContent>
       </Header>
-     
-      <Car data={carData} onPress={handleDetails}/>
+      {loading ? (
+       <Load/>
+      ) : (
+        <CarList
+          data={cars}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => <Car data={item} onPress={handleDetails} />}
+        ></CarList>
+      )}
     </Container>
   );
 }
